a51e394dd8e6519036e906a5ba08e40c60b11ae3
khanhdat1904 <24022287@vnu.edu.vn>
2025-03-01 07:29:55 +0700

    Add files via upload

---
+++ bt1dsa.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[1005];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int count =0;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<=n;j++){
+            if(a[i]=a[j]){
+                count++;
+            }
+        }
+    }
+    cout<<count;
+    return 0;
+}
